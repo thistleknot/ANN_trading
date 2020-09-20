28fd988915f612543c53711cf0ffed5f2094b31d
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-09-20 23:03:30 +0000

    initialAnnModified4Regression.R is a good R regression neural network

--- initialAnnModified4Regression.R
+++ initialAnnModified4Regression.R
@@ -11,6 +11,8 @@ sigmoid <- function(z) {
   g
 }
 
+file.remove("data.txt")
+write(paste(c("i", "loss", "mse"),collapse = ","),file="data.txt",append=TRUE)
 
 #modified from: https://github.com/thistleknot/Ann-v2/blob/master/myNueralNet.cpp
 
@@ -35,6 +37,8 @@ predict.dnn <- function(model, data = X.test) {
   return(score)
   
 }
+
+#fileConn<-file("output.txt")
 
 # Train: build and train a 2-layers neural network 
 train.dnn <- function(x, y, traindata=data, testdata=NULL,
@@ -162,9 +166,15 @@ train.dnn <- function(x, y, traindata=data, testdata=NULL,
         # updated: 10.March.2016
         diff <- labs - testdata[,y]
         mse <- (t(diff) %*% diff) / (2 * N)
-        cat(i, loss, mse, "\n")
+        #writeLines(paste(i, loss, mse, "\n"), fileConn)
+        write(paste(c(i, loss, mse),collapse = ","),file="data.txt",append=TRUE)
+        #writeLines(paste("Hello","World"), fileConn)
+        
+        #cat(i, loss, mse, "\n")
         } else {
-        cat(i, loss, "\n")
+          write(paste(c(i, loss, "\n"),collapse = ","),file="data.txt",append=TRUE)
+          #writeLines(paste(i, loss, "\n"), fileConn)
+        #cat(i, loss, "\n")
       }
     }
     
@@ -181,6 +191,7 @@ train.dnn <- function(x, y, traindata=data, testdata=NULL,
     #should be adding gradients
     
     #error * output is what? (closest to node delta, but only after multiplied by weight)
+    #Sum * Node Delta is in Dave Miller's Code for 
     dW2 <- t(hidden.layer) %*% dscores 
     #why is it adding derivatives to the bias delta?
     db2 <- colSums(dscores)
@@ -262,6 +273,7 @@ traindata <- predict(trainParam,traindata)
 testdata <- predict(trainParam,testdata)
 
 model <- train.dnn(x=2:10, y=1, traindata, testdata, hidden=6, maxit=2000, display=50)
+#close(fileConn)
 
 test.predictions <- predict.dnn(model, data[-samp,-1])
 
@@ -273,73 +285,30 @@ diff <- test.predictions - test[,1]
 mse <- (t(diff) %*% diff) / (2 * m)
 mse
 
-if(FALSE)
-{
-  # Visualization
-  # the output from screen, copy and paste here.
-  data1 <- ("i loss mse
-        50 0.1815593 0.1955971 
-100 0.1544014 0.1645331 
-          150 0.1359382 0.1447487 
-          200 0.1193891 0.1273547 
-          250 0.1043342 0.1116742 
-          300 0.09071633 0.09756991 
-          350 0.07861059 0.08506636 
-          400 0.06809837 0.07421481 
-          450 0.05920095 0.06502153 
-          500 0.05185695 0.05741881 
-          550 0.04593181 0.05126896 
-          600 0.04124379 0.04638704 
-          650 0.03759254 0.0425684 
-          700 0.034782 0.03961181 
-          750 0.032635 0.03733479 
-          800 0.03100038 0.0355813 
-          850 0.02975462 0.03422377 
-          900 0.02880007 0.03316147 
-          950 0.02806161 0.03231709 
-          1000 0.02748264 0.03163272 
-          1050 0.02702125 0.03106581 
-          1100 0.02664687 0.03058564 
-          1150 0.02633737 0.03017033 
-          1200 0.02607689 0.02980444 
-          1250 0.025854 0.02947713 
-          1300 0.02566047 0.02918074 
-          1350 0.02549033 0.02890983 
-          1400 0.02533914 0.02866039 
-          1450 0.02520355 0.02842944 
-          1500 0.02508101 0.02821464 
-          1550 0.0249695 0.02801411 
-          1600 0.0248674 0.02782629 
-          1650 0.02477342 0.02764986 
-          1700 0.02468646 0.02748365 
-          1750 0.02460564 0.02732667 
-          1800 0.0245302 0.02717803 
-          1850 0.0244595 0.02703695 
-          1900 0.02439301 0.02690273 
-          1950 0.02433024 0.02677474 
-          2000 0.02427081 0.02665246 ")
-  
-  data.v <- read.table(text=data1, header=T)
-  par(mar=c(5.1, 4.1, 4.1, 4.1))
-  plot(x=data.v$i, y=10*(data.v$loss), type="o", col="blue", pch=16, 
-       main="IRIS loss and mse by 2-layers DNN",
-       ylim=c(0, 2.0),
-       xlab="",
-       ylab="",
-       axe =F)
-  lines(x=data.v$i, y=10*data.v$mse, type="o", col="red", pch=1)
-  box()
-  axis(1, at=seq(0,2000,by=200))
-  axis(4, at=seq(0.2,2.0,by=0.1))
-  axis(2, at=seq(0.2,2.0,by=0.1))
-  mtext("training step", 1, line=3)
-  mtext("loss of training set (*10)", 2, line=2.5)
-  mtext("mse of testing set (*10)", 4, line=2)
-  
-  legend("bottomleft", 
-         legend = c("loss", "mse"),
-         pch = c(16,1),
-         col = c("blue","red"),
-         lwd=c(1,1)
-  )
-}
+# Visualization
+
+data1 <- read.csv("data.txt",header =T)
+
+data.v <- data1
+par(mar=c(5.1, 4.1, 4.1, 4.1))
+plot(x=data.v$i, y=10*(data.v$loss), type="o", col="blue", pch=16, 
+     main="IRIS loss and mse by 2-layers DNN",
+     ylim=c(0, 2.0),
+     xlab="",
+     ylab="",
+     axe =F)
+lines(x=data.v$i, y=10*data.v$mse, type="o", col="red", pch=1)
+box()
+axis(1, at=seq(0,2000,by=200))
+axis(4, at=seq(0.2,2.0,by=0.1))
+axis(2, at=seq(0.2,2.0,by=0.1))
+mtext("training step", 1, line=3)
+mtext("loss of training set (*10)", 2, line=2.5)
+mtext("mse of testing set (*10)", 4, line=2)
+
+legend("bottomleft", 
+       legend = c("loss", "mse"),
+       pch = c(16,1),
+       col = c("blue","red"),
+       lwd=c(1,1)
+)
